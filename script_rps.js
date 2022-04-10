609650b8c7497a0d5ec05b6d56b1bc5c410c0959
Ian <ian.forest22@gmail.com>
2022-04-09 21:31:42 -0700

    add ability to generate player and cpu choice then compare outcomes

--- script_rps.js
+++ script_rps.js
@@ -4,10 +4,29 @@ let cpuChoice
 
 function computerPlay() {
     let roll = Math.floor((Math.random()) * 3)
-    if (roll === 0) { cpuChoice = 'computer picks rock' }
-    if (roll === 1) { cpuChoice = 'computer picks paper' }
-    if (roll === 2) { cpuChoice = 'computer picks scissors' }
+    if (roll === 0) { cpuChoice = 'rock' }
+    if (roll === 1) { cpuChoice = 'paper' }
+    if (roll === 2) { cpuChoice = 'scissors' }
 }
 
 computerPlay()
+
+let playerChoice = prompt('Choose rock paper or scissors')
+
+if (playerChoice === "rock" && cpuChoice === "rock") { console.log("You both chose rock, you tie.") }
+else if (playerChoice === "rock" && cpuChoice === "scissors") { console.log("You chose rock and they chose scissors, you win!") }
+else if (playerChoice === "rock" && cpuChoice === "paper") { console.log("You chose rock and they chose paper, you lose!") }
+else if (playerChoice === "paper" && cpuChoice === "paper") { console.log("You both chose paper, you tie.") }
+else if (playerChoice === "paper" && cpuChoice === "rock") { console.log("You chose paper and they chose rock, you win!") }
+else if (playerChoice === "paper" && cpuChoice === "scissors") { console.log("You chose paper and they chose scissors, you lose!") }
+else if (playerChoice === "scissors" && cpuChoice === "scissors") { console.log("You both chose scissors, you tie.") }
+else if (playerChoice === "scissors" && cpuChoice === "paper") { console.log("You chose scissors and they chose paper, you win!") }
+else if (playerChoice === "scissors" && cpuChoice === "rock") { console.log("You chose scissors and they chose rock, you lose!") }
+
+
+
+
+
+
+
 
