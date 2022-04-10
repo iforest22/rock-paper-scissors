28ece9073803327bb220c5d8ec4b61303ffbb5b3
Ian <ian.forest22@gmail.com>
2022-04-10 15:35:29 -0700

    add playRound() and game() functions

--- script_rps.js
+++ script_rps.js
@@ -1,29 +1,37 @@
 
+function playRound() {
+    let cpuChoice
 
-let cpuChoice
+    function computerPlay() {
+        let roll = Math.floor((Math.random()) * 3)
+        if (roll === 0) { cpuChoice = 'rock' }
+        if (roll === 1) { cpuChoice = 'paper' }
+        if (roll === 2) { cpuChoice = 'scissors' }
+    }
 
-function computerPlay() {
-    let roll = Math.floor((Math.random()) * 3)
-    if (roll === 0) { cpuChoice = 'rock' }
-    if (roll === 1) { cpuChoice = 'paper' }
-    if (roll === 2) { cpuChoice = 'scissors' }
+    computerPlay()
+
+    let playerChoice = prompt('Choose rock paper or scissors')
+    playerChoice = playerChoice.toLowerCase()
+
+    if (playerChoice === "rock" && cpuChoice === "rock") { console.log("You both chose rock, you tie.") }
+    else if (playerChoice === "rock" && cpuChoice === "scissors") { console.log("You chose rock and they chose scissors, you win!") }
+    else if (playerChoice === "rock" && cpuChoice === "paper") { console.log("You chose rock and they chose paper, you lose!") }
+    else if (playerChoice === "paper" && cpuChoice === "paper") { console.log("You both chose paper, you tie.") }
+    else if (playerChoice === "paper" && cpuChoice === "rock") { console.log("You chose paper and they chose rock, you win!") }
+    else if (playerChoice === "paper" && cpuChoice === "scissors") { console.log("You chose paper and they chose scissors, you lose!") }
+    else if (playerChoice === "scissors" && cpuChoice === "scissors") { console.log("You both chose scissors, you tie.") }
+    else if (playerChoice === "scissors" && cpuChoice === "paper") { console.log("You chose scissors and they chose paper, you win!") }
+    else if (playerChoice === "scissors" && cpuChoice === "rock") { console.log("You chose scissors and they chose rock, you lose!") }
 }
 
-computerPlay()
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound()
+    }
+}
 
-let playerChoice = prompt('Choose rock paper or scissors')
-
-if (playerChoice === "rock" && cpuChoice === "rock") { console.log("You both chose rock, you tie.") }
-else if (playerChoice === "rock" && cpuChoice === "scissors") { console.log("You chose rock and they chose scissors, you win!") }
-else if (playerChoice === "rock" && cpuChoice === "paper") { console.log("You chose rock and they chose paper, you lose!") }
-else if (playerChoice === "paper" && cpuChoice === "paper") { console.log("You both chose paper, you tie.") }
-else if (playerChoice === "paper" && cpuChoice === "rock") { console.log("You chose paper and they chose rock, you win!") }
-else if (playerChoice === "paper" && cpuChoice === "scissors") { console.log("You chose paper and they chose scissors, you lose!") }
-else if (playerChoice === "scissors" && cpuChoice === "scissors") { console.log("You both chose scissors, you tie.") }
-else if (playerChoice === "scissors" && cpuChoice === "paper") { console.log("You chose scissors and they chose paper, you win!") }
-else if (playerChoice === "scissors" && cpuChoice === "rock") { console.log("You chose scissors and they chose rock, you lose!") }
-
-
+game()
 
 
 
